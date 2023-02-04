d77e7e8c2deb780db21e4450acc1218a533c49be
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-02-04 07:26:23 -0400

    Almost completed challenge. Need to work on the view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address
+    # rating: rand(1..5)
+  )
+  puts "Restaurant with id: #{restaurant.id} has been created"
+end
